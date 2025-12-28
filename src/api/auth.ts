bab922f425575d96cf01dc950db26111dbb051c3
Neil Barot <neilbarot5@gmail.com>
2025-12-28 02:43:40 -0500

    added gunicorn

--- src/api/auth.ts
+++
@@ -1,7 +0,0 @@
-export async function login(email: string, password: string) {
-  return fetch("http://127.0.0.1:5000/api/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password })
-  }).then(res => res.json());
-}
