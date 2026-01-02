fedade3c0950a88f728f4676a2b9628fb8fe5c86
Neil Barot <neilbarot5@gmail.com>
2026-01-01 19:55:31 -0500

    backend working successfully

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,4 +20,5 @@ def home():
     return {"message": "Flask is working with blueprints!"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
