d4cb89cc16d290100da24153d5f3dface200f24b
Neil Barot <neilbarot5@gmail.com>
2026-01-07 00:05:24 -0500

    bug fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/globals.css"; // adjust path if your css file name differs
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
